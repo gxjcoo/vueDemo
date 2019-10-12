fea3d032c19231617ad08b975cbfd54d667751ac
gxjcoo <gxjcoo@gmail.com>
2019-10-12 17:56:50 +0800

    table active

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,26 +1,27 @@
 <template>
   <div class="box">
- 
-  <el-table
-    :data="tableData"
-    height="250"
-    border
-    style="width: 100%;">
-    <el-table-column
-      prop="date"
-      label="日期"
-      width="180">
-    </el-table-column>
-    <el-table-column
-      prop="name"
-      label="姓名"
-      width="180">
-    </el-table-column>
-    <el-table-column
-      prop="address"
-      label="地址">
-    </el-table-column>
-  </el-table>
+    <h2>基于el-table的动态表格</h2>
+    <div style="width:30rem;margin:0 auto">
+<el-table :data="tableData" height="250" border style="width: 100%;">
+      <el-table-column prop="date" label="日期" width="180"></el-table-column>
+      <el-table-column prop="name" label="姓名" width="180"></el-table-column>
+      <el-table-column prop="address" label="地址"></el-table-column>
+    </el-table>
+    </div>
+    <div>
+      <h2>使用循环动态生成table</h2>
+      <div class="table">
+        <!-- 表头 -->
+        <p class="heard">
+          <span v-for="item in tableHeader" :key="item">{{item}}</span>
+        </p>
+        <!-- 列表 -->
+        <div class="tbody">
+          <!-- 行 -->
+          <p class="tr"></p>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -28,11 +29,8 @@
 
 
 <script>
-  export default {
-    data() {
-      return {
-        tableData: [{
-          date: '2016-05-03',
+  let tableData=[{
+          date: '2016-05-01',
           name: '王小虎',
           address: '上海市普陀区金沙江路 1518 弄'
         }, {
@@ -40,52 +38,213 @@
           name: '王小虎',
           address: '上海市普陀区金沙江路 1518 弄'
         }, {
-          date: '2016-05-04',
+          date: '2016-05-03',
           name: '王小虎',
           address: '上海市普陀区金沙江路 1518 弄'
         }, {
+          date: '2016-05-04',
+          name: '王小虎',
+          address: '上海市普陀区金沙江路 1518 弄'
+        },{
           date: '2016-05-01',
           name: '王小虎',
           address: '上海市普陀区金沙江路 1518 弄'
-        }]
+        }, {
+          date: '2016-05-02',
+          name: '王小虎',
+          address: '上海市普陀区金沙江路 1518 弄'
+        }, {
+          date: '2016-05-03',
+          name: '王小虎',
+          address: '上海市普陀区金沙江路 1518 弄'
+        }, {
+          date: '2016-05-04',
+          name: '王小虎',
+          address: '上海市普陀区金沙江路 1518 弄'
+        },{
+          date: '2016-05-01',
+          name: '王小虎',
+          address: '上海市普陀区金沙江路 1518 弄'
+        }, {
+          date: '2016-05-02',
+          name: '王小虎',
+          address: '上海市普陀区金沙江路 1518 弄'
+        }, {
+          date: '2016-05-03',
+          name: '王小虎',
+          address: '上海市普陀区金沙江路 1518 弄'
+        }, {
+          date: '2016-05-04',
+          name: '王小虎',
+          address: '上海市普陀区金沙江路 1518 弄'
+        },{
+          date: '2016-05-01',
+          name: '王小虎',
+          address: '上海市普陀区金沙江路 1518 弄'
+        }, {
+          date: '2016-05-02',
+          name: '王小虎',
+          address: '上海市普陀区金沙江路 1518 弄'
+        }, {
+          date: '2016-05-03',
+          name: '王小虎',
+          address: '上海市普陀区金沙江路 1518 弄'
+        }, {
+          date: '2016-05-04',
+          name: '王小虎',
+          address: '上海市普陀区金沙江路 1518 弄'
+        },{
+          date: '2016-05-01',
+          name: '王小虎',
+          address: '上海市普陀区金沙江路 1518 弄'
+        }, {
+          date: '2016-05-02',
+          name: '王小虎',
+          address: '上海市普陀区金沙江路 1518 弄'
+        }, {
+          date: '2016-05-03',
+          name: '王小虎',
+          address: '上海市普陀区金沙江路 1518 弄'
+        }, {
+          date: '2016-05-04',
+          name: '王小虎',
+          address: '上海市普陀区金沙江路 1518 弄'
+        },{
+          date: '2016-05-01',
+          name: '王小虎',
+          address: '上海市普陀区金沙江路 1518 弄'
+        }, {
+          date: '2016-05-02',
+          name: '王小虎',
+          address: '上海市普陀区金沙江路 1518 弄'
+        }, {
+          date: '2016-05-03',
+          name: '王小虎',
+          address: '上海市普陀区金沙江路 1518 弄'
+        }, {
+          date: '2016-05-04',
+          name: '王小虎',
+          address: '上海市普陀区金沙江路 1518 弄'
+        },{
+          date: '2016-05-01',
+          name: '王小虎',
+          address: '上海市普陀区金沙江路 1518 弄'
+        }, {
+          date: '2016-05-02',
+          name: '王小虎',
+          address: '上海市普陀区金沙江路 1518 弄'
+        }, {
+          date: '2016-05-03',
+          name: '王小虎',
+          address: '上海市普陀区金沙江路 1518 弄'
+        }, {
+          date: '2016-05-04',
+          name: '王小虎',
+          address: '上海市普陀区金沙江路 1518 弄'
+        },{
+          date: '2016-05-01',
+          name: '王小虎',
+          address: '上海市普陀区金沙江路 1518 弄'
+        }, {
+          date: '2016-05-02',
+          name: '王小虎',
+          address: '上海市普陀区金沙江路 1518 弄'
+        }, {
+          date: '2016-05-03',
+          name: '王小虎',
+          address: '上海市普陀区金沙江路 1518 弄'
+        }, {
+          date: '2016-05-04',
+          name: '王小虎',
+          address: '上海市普陀区金沙江路 1518 弄'
+        },{
+          date: '2016-05-01',
+          name: '王小虎',
+          address: '上海市普陀区金沙江路 1518 弄'
+        }, {
+          date: '2016-05-02',
+          name: '王小虎',
+          address: '上海市普陀区金沙江路 1518 弄'
+        }, {
+          date: '2016-05-03',
+          name: '王小虎',
+          address: '上海市普陀区金沙江路 1518 弄'
+        }, {
+          date: '2016-05-04',
+          name: '王小虎',
+          address: '上海市普陀区金沙江路 1518 弄'
+        }];
+  export default {
+    data() {
+      return {
+        interval:'',
+        tableData: [],
+        tableHeader:["日期","姓名","地址"]
       }
     },
-    methods:{
-
-change() {
-
-//change、paly表格数据动态滚动
-console.log(this.tableData[0])
-this.tableData.push(this.tableData[0]);//把第一条数据插入数组最有一条
-
-this.tableData.shift();//删除数组中第一条数据
-
+methods:{
+  change() {
+    if(tableData.length>0){
+      //change、paly表格数据动态滚动
+      this.tableData.unshift(tableData[0]);//把第一条数据插入数组最有一条
+      tableData.shift();//删除数组中第一条数据
+      }else{
+        console.log("没数据了")
+        window.clearInterval(this.interval)
+        this.interval=null
+    }
+  },
+  play() {
+  this.interval = setInterval(this.change, 2000);//每两秒执行一次插入删除操作
+  }
 },
-
-play() {
-
-setInterval(this.change, 2000);//每两秒执行一次插入删除操作
-
-}
-
-},
-
 created(){
-
-this.play()
-
-}
-
+  this.play()
+},
+destroyed() {
+  window.clearInterval(this.interval)
+  this.interval=null
+},
   }
 </script>
 <style lang="less">
-.el-table__row{
-transition:add 1s ease 0s;
 
+.box {
+  width: 100%;
+  height: 100vh;
 }
-.box{
-  width:100%;
-  height:100vh;
-   background: #000;
+
+//自写table
+.table{
+  color:#fff;
+  background: #000;
+.heard{
+  span{
+    margin-right:8rem;
+  }
 }
+}
+//el-table
+.el-table__row:nth-child(1){
+  animation: donghua 2s infinite ;
+}
+.el-table__body{
+  position: absolute;
+  animation: tableDonghua 1s infinite;
+}
+@keyframes donghua {
+  0%{
+    opacity: 0.51;
+  }
+  100%{
+    opacity: 1;
+  }
+}
+@keyframes tableDonghua {
+  0%{
+  top:-1.8rem
+  }
+  100%{
+  top:0rem
+  }}
 </style>
