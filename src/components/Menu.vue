e7b2d070239b86c0d57d83dce5acb75aa18a7022
gxjcoo <gxjcoo@gmail.com>
2019-10-27 12:13:27 +0800

    upd

---
+++ src/components/Menu.vue
@@ -0,0 +1,127 @@
+<template>
+    <div>
+          <nav class="main-menu">
+            <div class="logo"></div>
+            <div class="settings"></div>
+            <ul>
+                <li>
+                    <a href="#">
+                        <i class="fa fa-home fa-lg"></i>
+                        <span class="nav-text">Home</span>
+                    </a>
+                </li>
+                <li class="has-subnav">
+                    <a href="#">
+                        <i class="fa fa-clock-o fa-lg"></i>
+                        <span class="nav-text">
+                            News
+                        </span>
+                    </a>
+
+                </li>
+
+                <li>
+                    <a href="#">
+                        <i class="fa fa-desktop fa-lg"></i>
+                        <span class="nav-text">
+                            Technology
+                        </span>
+                    </a>
+                </li>
+                <li class="has-subnav">
+                    <a href="#">
+                        <i class="fa fa-plane fa-lg"></i>
+                        <span class="nav-text">
+                            Travel
+                        </span>
+                    </a>
+
+                </li>
+
+                <li>
+                    <a href="#">
+                        <i class="fa fa-shopping-cart"></i>
+                        <span class="nav-text">
+                            Shopping
+                        </span>
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                        <i class="fa fa-microphone fa-lg"></i>
+                        <span class="nav-text">
+                            Film & Music
+                        </span>
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                        <i class="fa fa-flask fa-lg"></i>
+                        <span class="nav-text">
+                            Web Tools
+                        </span>
+                    </a>
+                </li>
+
+                <li>
+                    <a href="#">
+                        <i class="fa fa-picture-o fa-lg"></i>
+                        <span class="nav-text">
+                            Art & Design
+                        </span>
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                        <i class="fa fa-align-left fa-lg"></i>
+                        <span class="nav-text">
+                            Magazines
+                        </span>
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                        <i class="fa fa-gamepad fa-lg"></i>
+                        <span class="nav-text">
+                            Games
+                        </span>
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                        <i class="fa fa-glass fa-lg"></i>
+                        <span class="nav-text">
+                            Life & Style
+                        </span>
+                    </a>
+                </li>
+                <li class="has-subnav">
+                    <a href="#">
+                        <i class="fa fa-rocket fa-lg"></i>
+                        <span class="nav-text">
+                            Fun
+                        </span>
+                    </a>
+
+                </li>
+            </ul>
+
+            <ul class="logout">
+                <li>
+                    <a href="#">
+                        <i class="fa fa-lightbulb-o fa-lg"></i>
+                        <span class="nav-text">
+                            BLOG
+                        </span>
+
+                    </a>
+                </li>
+            </ul>
+        </nav>
+        <div style="text-align:center;clear:both">
+        </div>
+    </div>
+</template>
+<style lang="less" scoped>
+ @import "../assets/css/menu.css";
+</style>
