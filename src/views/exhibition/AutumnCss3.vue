d2a643bf43c7f177a02d1249ce1c902d9325397c
gxjcoo <gxjcoo@gmail.com>
2019-11-06 19:50:26 +0800

    aug

--- src/views/exhibition/AutumnCss3.vue
+++ src/views/exhibition/AutumnCss3.vue
@@ -1,24 +1,12 @@
 <template>
   <div class="bg">
-    <div class="box">
+    <div class="boxAut">
       <figure>
         <figcaption>Autumn in HangZhou</figcaption>
       </figure>
     </div>
     
-    <div class="box2">
-            <!-- 一个div中放全部图片 -->
-      <div class="content" :style="a==img.length?'opacity:1':'opacity:0'">
-        <div class="item" :style="itemStyle()" v-for="item in img"  :key="item">
-          <img :src="`/img/${item}.jpeg`" />
-        </div>
-      </div>
-      <!-- 每个div放一个图片 -->
-      <div class="content" :style="index == a?'opacity:1':'opacity:0'" v-for="(item,index) in img "  :key="item">
-        <img :src="`/img/${item}.jpeg`" width="100%" />
-      </div>
 
-    </div>
     
   </div>
 </template>
@@ -26,68 +14,26 @@
 export default {
   data() {
     return {
-      img: ["autumn1", "lang_dark", "autumn3"],
-      a:'',
+     
     };
   },
   methods: {
-    itemStyle() {
-      return { width: 100 / this.img.length + "%" };
-    },
-    contentStyle(){
-
-    }
+   
   },
   mounted(){
-    this.a =this.img.length
-    let i = 0;
-    setInterval(()=>{
-      i++
-      this.a = i%(this.img.length+1)
-      console.log(this.a)
-    },2000)
+ 
   }
 };
 </script>
 <style lang="less">
-.box2 {
-  background: #fff;
-  height: 8rem;
-  width: 12rem;
-  margin: 4rem auto;
-  // overflow: hidden;
-  position: relative;
-  .content {
-    height: 100%;
-    width: 100%;
-    overflow: hidden;
-    position: absolute;
-  transition: all 1s ;
-    .item {
-      width: 100%;
-      height: 100%;
-      float: left;
-      img {
-        height: 100%;
-      }
-    }
-  }
-}
-@keyframes opa {
-  0%{
-    opacity: 1;
-  }
-  100%{
-    opacity: 0;
-  }
-}
+
 .bg {
   width: 100%;
   height: 100%;
   overflow: hidden;
   background: linear-gradient(hsl(80, 51%, 8%), #e0d7a3);
 }
-.box {
+.boxAut {
   perspective: 1000px;
   width: 33%;
   margin: 0 auto;
@@ -134,10 +80,10 @@ figure:before {
   transform: rotateX(95deg) translateZ(-80px) scale(0.75);
   transform-origin: inherit;
 }
-.box:hover figure {
+.boxAut:hover figure {
   transform: rotateX(75deg) translateZ(5vw);
 }
-.box:hover figure:before {
+.boxAut:hover figure:before {
   box-shadow: 0 0 25px 25px rgba(0, 0, 0, 0.5),
     inset 0 0 250px 250px rgba(0, 0, 0, 0.5);
   transform: rotateX(-5deg) translateZ(-80px) scale(1);
--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -1,15 +1,16 @@
 <template>
-  <Sfq :imgs="i"></Sfq>
+  <Acc></Acc>
 </template>
 <script>
 import Sfq from "../component/shoufengqin.vue"
+import Acc from "../exhibition/AutumnCss3.vue"
 export default {
 data() {
   return {
     i:[1,2,6,9,7,8,3,4]
   }
 },
-  components:{Sfq}
+  components:{Sfq,Acc}
 };
 </script>
 <style lang="less">
