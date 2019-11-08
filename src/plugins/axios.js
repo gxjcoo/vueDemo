490b513e414139f507d06be945a680c2394351a4
gxjcoo <gxjcoo@gmail.com>
2019-11-08 17:32:09 +0800

    全局loading

--- src/plugins/axios.js
+++ src/plugins/axios.js
@@ -1,6 +1,8 @@
 "use strict";
 
 import Vue from 'vue';
+import { Message,Loading } from 'element-ui';
+import _ from 'lodash';
 import axios from "axios";
 
 // Full config:  https://github.com/axios/axios#request-config
@@ -9,49 +11,111 @@ import axios from "axios";
 // axios.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded';
 
 let config = {
-  baseURL:'/api'
+  baseURL:'/api',
+  timeout:40000 //超时时长
+
   // baseURL: process.env.baseURL || process.env.apiUrl || ""
   // timeout: 60 * 1000, // Timeout
   // withCredentials: true, // Check cross-site Access-Control
 };
+const http = axios.create(config);
 
-const _axios = axios.create(config);
+//loading对象
+let loading;
 
-_axios.interceptors.request.use(
-  function(config) {
-    // Do something before request is sent
-    return config;
-  },
-  function(error) {
-    // Do something with request error
-    return Promise.reject(error);
-  }
-);
+//当前正在请求的数量
+let needLoadingRequestCount = 0;
 
-// Add a response interceptor
-_axios.interceptors.response.use(
-  function(response) {
-    // Do something with response data
+//显示loading
+function showLoading(target) {
+// 后面这个判断很重要，因为关闭时加了抖动，此时loading对象可能还存在，
+// 但needLoadingRequestCount已经变成0.避免这种情况下会重新创建个loading
+if (needLoadingRequestCount === 0 && !loading) {
+  loading = Loading.service({
+    lock: true,
+    //text: "Loading...",
+    background: 'rgba(0, 0, 0, 0.5)',
+    target: target || ".cont"
+  });
+}
+needLoadingRequestCount++;
+}
+
+//隐藏loading
+function hideLoading() {
+needLoadingRequestCount--;
+needLoadingRequestCount = Math.max(needLoadingRequestCount, 0); //做个保护
+if (needLoadingRequestCount === 0) {
+  //关闭loading
+  toHideLoading();
+}
+}
+
+//防抖：将 300ms 间隔内的关闭 loading 便合并为一次。防止连续请求时， loading闪烁的问题。
+var toHideLoading = _.debounce(()=>{
+    loading.close();
+    loading = null;
+  }, 300);
+
+//添加请求拦截器
+http.interceptors.request.use(config => {
+//判断当前请求是否设置了不显示Loading
+if(config.headers.showLoading !== false){
+  showLoading(config.headers.loadingTarget);
+}
+return config;
+}, err => {
+//判断当前请求是否设置了不显示Loading
+if(config.headers.showLoading !== false){
+  hideLoading();
+}
+Message.error('请求超时!');
+return Promise.resolve(err);
+});
+
+//响应拦截器
+http.interceptors.response.use(
+  response => {
+    //判断当前请求是否设置了不显示Loading（不显示自然无需隐藏）
+    if(response.config.headers.showLoading !== false){
+      hideLoading();
+    }
     return response;
   },
-  function(error) {
-    // Do something with response error
+  error => {
+    //判断当前请求是否设置了不显示Loading（不显示自然无需隐藏）
+    if(error.config.headers.showLoading !== false){
+      hideLoading();
+    }
+    if(error.response && error.response.data && error.response.data.message) {
+      var jsonObj = JSON.parse(error.response.data.message);
+      Message.error(jsonObj.message);
+    }else{
+      Message.error(error.message);
+    }
     return Promise.reject(error);
   }
 );
 
+
+
+
+
+
+
+
 Plugin.install = function(Vue, options) {
-  Vue.axios = _axios;
-  window.axios = _axios;
+  Vue.axios = http;
+  window.axios = http;
   Object.defineProperties(Vue.prototype, {
     axios: {
       get() {
-        return _axios;
+        return http;
       }
     },
     $axios: {
       get() {
-        return _axios;
+        return http;
       }
     },
   });
--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -2,15 +2,24 @@
   <Acc></Acc>
 </template>
 <script>
-import Sfq from "../component/shoufengqin.vue"
 import Acc from "../exhibition/AutumnCss3.vue"
 export default {
 data() {
   return {
-    i:[1,2,6,9,7,8,3,4]
   }
 },
-  components:{Sfq,Acc}
+  components:{Acc},
+  methods:{
+    async  init(){
+ let  a = this.$axios.get('http://10.121.200.201:8080/stap/trafficAnalysis/internal?direction=1&startTime=1573142400000&endTime=1573193972946#/login').then(
+   setTimeout(()=>{},10000)
+ )
+    }
+  },
+  created(){
+this.init()
+   
+  }
 };
 </script>
 <style lang="less">
