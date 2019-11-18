66b490d8d31707029f4e08727f497c1c06cf5660
gxjcoo <gxjcoo@gmail.com>
2019-11-18 12:32:26 +0800

    mul page down

--- src/multiPage/page2.vue
+++ src/multiPage/page2.vue
@@ -1,6 +1,16 @@
 <template>
   <div id="app">
-    aaaaaaaaaaaaaaaaa
+    <h1>多页面展示</h1>
+    <h2>config文件中加入一下代码，按照代码进行目录构建</h2>
+    <p>
+    pages:{
+        index:{
+            entry:'src/main.js',
+            template:'public/index.html'
+        },
+	    page:'src/multiPage/page2.js',
+},
+    </p>
   </div>
 </template>
 
