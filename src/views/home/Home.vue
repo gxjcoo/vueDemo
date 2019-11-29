24d98767be6e14648f9014accc848fb17380356d
gxjcoo <gxjcoo@gmail.com>
2019-11-29 20:05:43 +0800

    require.context() 初体验

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -1,20 +1,35 @@
 <template>
-  <Acc></Acc>
+  <div id="app">
+    <li v-for="(img, index) in images" :key="index">
+      <p>img: {{img}}</p>
+      <img :src="imgUrl(img)" alt="">
+    </li>
+  </div>
 </template>
-<script>
-import Acc from "../exhibition/AutumnCss3.vue"
-export default {
-data() {
-  return {
-  }
-},
-  components:{Acc},
-  methods:{
-  
-  },
-  
-};
-</script>
-<style lang="less">
 
+<script>
+console.log(require.context)
+const req = require.context('../../../public/img/guojia', false, /(\.png|\.gif|\.jpg)$/)
+export default {
+  name: 'App',
+  data () {
+    return {
+      images: []
+    }
+  },
+  created () {
+    this.images = req.keys()
+  },
+  methods: {
+    imgUrl (path) {
+      return req(path)
+    }
+  }
+}
+</script>
+
+<style>
+img {
+  height: 30px;
+}
 </style>
