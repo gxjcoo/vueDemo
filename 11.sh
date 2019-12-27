4a82d6faefc95b2032d148f273050ee04cff322c
gxjcoo <gxjcoo@gmail.com>
2019-12-27 16:02:49 +0800

    test

--- 11.sh
+++ 11.sh
@@ -1,15 +1,9 @@
-#!/usr/bin/expect
 msg=$1
 if [ -n "$msg" ]; then
-   git add -A
+   git add .
    git commit -m"${msg}"
    git pull
    git push
-expect "Username"
-send "gjxcoo\r"
-expect "Password for 'https://gxjcoo@github.com':"
-send "zjk123\r"
-   interact 
 else
     echo "请添加注释再来一遍"
 fi
