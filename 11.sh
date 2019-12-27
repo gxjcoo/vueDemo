87f097ecfe6b5a1f0b85eb0acfaddb9859dcc6ba
gxjcoo <gxjcoo@gmail.com>
2019-12-27 15:15:42 +0800

    test

--- 11.sh
+++ 11.sh
@@ -3,8 +3,9 @@ if [ -n "$msg" ]; then
    git add -A
    git commit -m"${msg}"
    git pull
-   git status
-   echo "完成add、commit、pull，别忘了push"
+   git push
+   gxjcoo
+   guan0924.
 else
     echo "请添加注释再来一遍"
 fi
