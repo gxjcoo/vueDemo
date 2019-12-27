ad91b8556782f112c35549ba1acfbc9634888faa
dptechstap <gxjcoo@gmail.com>
2019-12-27 15:37:11 +0800

    tet

--- 11.sh
+++ 11.sh
@@ -1,11 +1,15 @@
+#!/usr/bin/expect
 msg=$1
 if [ -n "$msg" ]; then
    git add -A
    git commit -m"${msg}"
    git pull
-   git push
-   echo 'gxjcoo'
-   echo 'guan0924.'
+  git push
+ expect "Username for 'https://github.com':"
+send "gjxcoo"
+expect "Password for 'https://gxjcoo@github.com':"
+send "zjk123\r"
+   
 else
     echo "请添加注释再来一遍"
 fi
