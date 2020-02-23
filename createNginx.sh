244a9d7a1509323b81322142928394a979ff3da3
gxjcoo <gxjcoo@gmail.com>
2020-02-23 20:58:12 +0800

    add docker生成nginx脚本

---
+++ createNginx.sh
@@ -0,0 +1,5 @@
+sudo docker stop vuenginxapp || true \
+ && sudo docker rm vuenginxapp || true \
+ && sudo cd /root/docker/jenkins_home/workspace/vuedemo  \
+ && sudo docker image build ./  -t vuenginxapp  . \
+ && sudo docker run -d -p 8083:80 --name vuenginxapp -v /root/docker/jenkins_home/workspace/vuedemo/dist:/usr/share/nginx/html -v /root/docker/jenkins_home/workspace/vuedemo/nginx.conf:/etc/nginx/nginx.conf vuenginxapp
