2afca7e2a2a872b7d9a3c51a25ebb716128802b9
gxjcoo <gxjcoo@gmail.com>
2020-02-23 17:52:26 +0800

    upd

--- src/App.vue
+++ src/App.vue
@@ -6,7 +6,12 @@
 
 <script>
 import Layout from './components/Layout.vue'
-
+import axios from 'axios';
+axios.get('/api', {
+  params: {},
+}).then((res) => {
+  console.log(res);
+});
 export default {
   name: 'app',
   components: {
--- src/plugins/axios.js
+++ src/plugins/axios.js
@@ -5,10 +5,7 @@ import axios from "axios";
 import { Message,Loading } from 'element-ui';
 import _ from 'lodash';
 
-// Full config:  https://github.com/axios/axios#request-config
-// axios.defaults.baseURL = process.env.baseURL || process.env.apiUrl || '';
-// axios.defaults.headers.common['Authorization'] = AUTH_TOKEN;
-// axios.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded';
+
 
 let config = {
   baseURL:'/api',
