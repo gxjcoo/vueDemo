87d5189585e9507190d0036a13a9f96092d93aa5
gxjcoo <gxjcoo@gmail.com>
2020-03-04 23:53:43 +0800

    upd

--- createNginx.sh
+++ createNginx.sh
@@ -1,7 +1,7 @@
-sudo docker stop vuenginxapp || true \
- && sudo docker rm vuenginxapp || true \
+sudo docker stop vuedemonginx || true \
+ && sudo docker rm vuedemonginx || true \
  && pwd \
- && sudo cd /root/docker/jenkins_home/workspace/vuedemo  \
+ && sudo cd /root/docker/jenkins_home/workspace/vueDemo  \
  && pwd \
- && sudo docker  build ./  -t vuenginxapp  . \
- && sudo docker run -d -p 9000:80 --name vuenginxapp -v /root/docker/jenkins_home/workspace/vuedemo/dist:/usr/share/nginx/html -v /root/docker/jenkins_home/workspace/vuedemo/nginx.conf:/etc/nginx/nginx.conf vuenginxapp
+ && sudo docker  build ./  -t vuedemonginx   \
+ && sudo docker run -d -p 9000:80 --name vuedemonginx -v /root/docker/jenkins_home/workspace/vueDemo/dist:/usr/share/nginx/html -v /root/docker/jenkins_home/workspace/vueDemo/nginx.conf:/etc/nginx/nginx.conf vuedemonginx
